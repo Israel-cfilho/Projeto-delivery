25620ab5f4408e9e38d053e7c9776edce991847a
israel filho <israel.filho@aluno.uepb.edu.br>
2024-03-18 17:29:06 -0300

    software delivery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css';
+import imagem from './img/duckDelivery.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Login() {
+    return (
+        <div className="wrapper">
+          <img src={imagem}></img>
+            <form action="">
+                <h1><span>DUCK'S</span> DELIVERY</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="Usuário" required />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Senha" required />
+                </div>
+
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />Lembrar de mim </label>
+                    <a href="#">Esqueceu a senha?</a>
+                </div>
+
+                <button type="submit" className="btn">Login</button>
+
+                <div className="register-link">
+                    <p>Não tem uma conta? <a href="#">Registre-se</a></p>
+                </div>
+            </form>
+        </div>
+    );
 }
 
-export default App;
+export default Login;
